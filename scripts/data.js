afb460f6705e80f143ff9a8fcb4329e27afd5835
CyMain <okikiolacyrus@gmail.com>
2026-02-23 12:24:46 +0100

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -39,22 +39,22 @@ export const skills = [
     {
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4.136 3.012h15.729l-1.431 16.15-6.451 1.826-6.414-1.826-1.433-16.15zm5.266 7.302-.173-2.035 7.533.002.173-1.963-9.87-.002.522 5.998h6.835l-.243 2.566-2.179.602-2.214-.605-.141-1.58H7.691l.247 3.123L12 17.506l4.028-1.08.558-6.111H9.402v-.001z"></path></svg>`,
         skillName:"HTML",
-        skillDesc:"Description about the skill goes here.",
+        skillDesc:"I use HTML to define the structure and hierarchy of a webpage, ensuring that content like headings, paragraphs, and images are organized logically.",
         skillProg:80
     },{
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4.192 3.143h15.615l-1.42 16.034-6.404 1.812-6.369-1.813L4.192 3.143zM16.9 6.424l-9.8-.002.158 1.949 7.529.002-.189 2.02H9.66l.179 1.913h4.597l-.272 2.62-2.164.598-2.197-.603-.141-1.569h-1.94l.216 2.867L12 17.484l3.995-1.137.905-9.923z"></path></svg>`,
         skillName:"CSS",
-        skillDesc:"Description about the skill goes here.",
+        skillDesc:"I use CSS to handle the visual presentation of the site.",
         skillProg:85
     },{
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><circle cx="12" cy="11.245" r="1.785"></circle><path d="m7.002 14.794-.395-.101c-2.934-.741-4.617-2.001-4.617-3.452 0-1.452 1.684-2.711 4.617-3.452l.395-.1.111.391a19.507 19.507 0 0 0 1.136 2.983l.085.178-.085.178c-.46.963-.841 1.961-1.136 2.985l-.111.39zm-.577-6.095c-2.229.628-3.598 1.586-3.598 2.542 0 .954 1.368 1.913 3.598 2.54.273-.868.603-1.717.985-2.54a20.356 20.356 0 0 1-.985-2.542zm10.572 6.095-.11-.392a19.628 19.628 0 0 0-1.137-2.984l-.085-.177.085-.179c.46-.961.839-1.96 1.137-2.984l.11-.39.395.1c2.935.741 4.617 2 4.617 3.453 0 1.452-1.683 2.711-4.617 3.452l-.395.101zm-.41-3.553c.4.866.733 1.718.987 2.54 2.23-.627 3.599-1.586 3.599-2.54 0-.956-1.368-1.913-3.599-2.542a20.683 20.683 0 0 1-.987 2.542z"></path><path d="m6.419 8.695-.11-.39c-.826-2.908-.576-4.991.687-5.717 1.235-.715 3.222.13 5.303 2.265l.284.292-.284.291a19.718 19.718 0 0 0-2.02 2.474l-.113.162-.196.016a19.646 19.646 0 0 0-3.157.509l-.394.098zm1.582-5.529c-.224 0-.422.049-.589.145-.828.477-.974 2.138-.404 4.38.891-.197 1.79-.338 2.696-.417a21.058 21.058 0 0 1 1.713-2.123c-1.303-1.267-2.533-1.985-3.416-1.985zm7.997 16.984c-1.188 0-2.714-.896-4.298-2.522l-.283-.291.283-.29a19.827 19.827 0 0 0 2.021-2.477l.112-.16.194-.019a19.473 19.473 0 0 0 3.158-.507l.395-.1.111.391c.822 2.906.573 4.992-.688 5.718a1.978 1.978 0 0 1-1.005.257zm-3.415-2.82c1.302 1.267 2.533 1.986 3.415 1.986.225 0 .423-.05.589-.145.829-.478.976-2.142.404-4.384-.89.198-1.79.34-2.698.419a20.526 20.526 0 0 1-1.71 2.124z"></path><path d="m17.58 8.695-.395-.099a19.477 19.477 0 0 0-3.158-.509l-.194-.017-.112-.162A19.551 19.551 0 0 0 11.7 5.434l-.283-.291.283-.29c2.08-2.134 4.066-2.979 5.303-2.265 1.262.727 1.513 2.81.688 5.717l-.111.39zm-3.287-1.421c.954.085 1.858.228 2.698.417.571-2.242.425-3.903-.404-4.381-.824-.477-2.375.253-4.004 1.841.616.67 1.188 1.378 1.71 2.123zM8.001 20.15a1.983 1.983 0 0 1-1.005-.257c-1.263-.726-1.513-2.811-.688-5.718l.108-.391.395.1c.964.243 2.026.414 3.158.507l.194.019.113.16c.604.878 1.28 1.707 2.02 2.477l.284.29-.284.291c-1.583 1.627-3.109 2.522-4.295 2.522zm-.993-5.362c-.57 2.242-.424 3.906.404 4.384.825.47 2.371-.255 4.005-1.842a21.17 21.17 0 0 1-1.713-2.123 20.692 20.692 0 0 1-2.696-.419z"></path><path d="M12 15.313c-.687 0-1.392-.029-2.1-.088l-.196-.017-.113-.162a25.697 25.697 0 0 1-1.126-1.769 26.028 26.028 0 0 1-.971-1.859l-.084-.177.084-.179c.299-.632.622-1.252.971-1.858.347-.596.726-1.192 1.126-1.77l.113-.16.196-.018a25.148 25.148 0 0 1 4.198 0l.194.019.113.16a25.136 25.136 0 0 1 2.1 3.628l.083.179-.083.177a24.742 24.742 0 0 1-2.1 3.628l-.113.162-.194.017c-.706.057-1.412.087-2.098.087zm-1.834-.904c1.235.093 2.433.093 3.667 0a24.469 24.469 0 0 0 1.832-3.168 23.916 23.916 0 0 0-1.832-3.168 23.877 23.877 0 0 0-3.667 0 23.743 23.743 0 0 0-1.832 3.168 24.82 24.82 0 0 0 1.832 3.168z"></path></svg>`,
         skillName:"ReactJS",
-        skillDesc:"Description about the skill goes here.",
+        skillDesc:"I use React to build component-based user interfaces that are fast and interactive.",
         skillProg:80
     },{
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M18.002 2.997H5.999A3.011 3.011 0 0 0 2.997 6v12.002a3.012 3.012 0 0 0 3.002 3.001h12.003a3.012 3.012 0 0 0 3.001-3.001V6a3.012 3.012 0 0 0-3.001-3.003zm-1.64 12.647c-.152.36-.389.68-.693.927a3.594 3.594 0 0 1-1.206.614c-.49.151-1.074.229-1.75.229H7.888V6.909h5.103c.943 0 1.7.213 2.267.646.569.436.854 1.082.854 1.958 0 .528-.13.983-.389 1.357-.259.373-.63.664-1.111.868v.034c.642.135 1.137.438 1.464.912.327.473.493 1.069.493 1.789 0 .405-.078.788-.225 1.147l.02.021v.003zm-2.084-2.589c-.309-.282-.739-.419-1.297-.419H9.737v3.276h3.253c.554 0 .991-.144 1.298-.435.308-.289.464-.701.464-1.229-.005-.518-.156-.919-.464-1.193h-.014.004zm-.451-2.119c.299-.251.444-.613.444-1.084 0-.526-.131-.902-.397-1.116-.264-.214-.646-.326-1.146-.326H9.727v2.898h3.001c.436.001.81-.127 1.099-.372z"></path></svg>`,
         skillName:"Bootstrap",
-        skillDesc:"Description about the skill goes here.",
+        skillDesc:"helps me implement a mobile-first design approach quickly, using its robust grid system and pre-styled components to keep my projects looking polished and consistent.",
         skillProg:70
     },{
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M9.585 11.692h4.328s2.432.039 2.432-2.35V5.391S16.714 3 11.936 3C7.362 3 7.647 4.983 7.647 4.983l.006 2.055h4.363v.617H5.92s-2.927-.332-2.927 4.282 2.555 4.45 2.555 4.45h1.524v-2.141s-.083-2.554 2.513-2.554zm-.056-5.74a.784.784 0 1 1 0-1.57.784.784 0 1 1 0 1.57z"></path><path d="M18.452 7.532h-1.524v2.141s.083 2.554-2.513 2.554h-4.328s-2.432-.04-2.432 2.35v3.951s-.369 2.391 4.409 2.391c4.573 0 4.288-1.983 4.288-1.983l-.006-2.054h-4.363v-.617h6.097s2.927.332 2.927-4.282-2.555-4.451-2.555-4.451zm-3.981 10.436a.784.784 0 1 1 0 1.57.784.784 0 1 1 0-1.57z"></path></svg>`,
@@ -64,7 +64,7 @@ export const skills = [
     },{
         skillIcon:`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7.533 12.249c-.011 1.985 1.445 3.168 3.768 2.63V9.618c-2.352-.716-3.758.733-3.768 2.631m3.839-10.238h3.199v15.143c-3.066.501-6.004.819-8.104-.355-2.705-1.513-2.923-6.319-.782-8.46 1.085-1.085 3.271-1.85 5.616-1.351V2.225c-.006-.101-.012-.202.071-.214m8.389 3.342h-3.199V2.011h3.199v3.342z"></path><path d="M19.761 7.044c-.003 2.356-.003 4.048-.003 6.911-.136 2.813-.104 5.052-1.135 6.398-.203.266-.634.652-.995.924-.303.228-.881.691-1.208.711-.331.021-1.18-.459-1.564-.64-.505-.237-.971-.553-1.493-.71 1.218-.754 2.372-1.32 2.844-2.844.41-1.326.355-3.247.355-5.119 0-1.849.009-3.998.009-5.63l3.19-.001z"></path></svg>`,
         skillName:"Django",
-        skillDesc:"Description about the skill goes here.",
+        skillDesc:"My framework of choice for building secure and scalable web apps.",
         skillProg:75
     },
 ]
@@ -73,19 +73,19 @@ export const services = [
     {
         serviceIcon:`<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 108.36 122.88" style="enable-background:new 0 0 108.36 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M0,0l9.86,110.59l44.25,12.29l44.37-12.3L108.36,0L0,0L0,0L0,0z M86.89,36.18h-51.9l1.24,13.89l49.43,0 l-3.72,41.64l-27.82,7.71l-27.79-7.71l-1.9-21.31h13.62l0.97,10.83l15.11,4.07l0.03-0.01l15.11-4.08l1.57-17.59l-47.01,0 l-3.65-41.02h67.94L86.89,36.18L86.89,36.18L86.89,36.18L86.89,36.18z"/></g></svg>`,
         serviceName:"Web Development",
-        serviceDesc:"Description about the service goes here."
+        serviceDesc:"I build functional, end-to-end web applications using React along with NodeJS, Django and FastAPI. From setting up the database to deploying the final product, I handle the full lifecycle of a small-to-medium project."
     },
     {
         serviceIcon:`<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 108.36 122.88" style="enable-background:new 0 0 108.36 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M0,0l9.86,110.59l44.25,12.29l44.37-12.3L108.36,0L0,0L0,0L0,0z M86.89,36.18h-51.9l1.24,13.89l49.43,0 l-3.72,41.64l-27.82,7.71l-27.79-7.71l-1.9-21.31h13.62l0.97,10.83l15.11,4.07l0.03-0.01l15.11-4.08l1.57-17.59l-47.01,0 l-3.65-41.02h67.94L86.89,36.18L86.89,36.18L86.89,36.18L86.89,36.18z"/></g></svg>`,
         serviceName:"Web Design",
-        serviceDesc:"Description about the service goes here."
+        serviceDesc:"Focused on simplicity and usability. I design layouts that aren't just pretty, but easy to navigate. I prioritize responsive design so your site looks great whether it's being viewed on a desktop or a smartphone."
     },{
         serviceIcon:`<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 108.36 122.88" style="enable-background:new 0 0 108.36 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M0,0l9.86,110.59l44.25,12.29l44.37-12.3L108.36,0L0,0L0,0L0,0z M86.89,36.18h-51.9l1.24,13.89l49.43,0 l-3.72,41.64l-27.82,7.71l-27.79-7.71l-1.9-21.31h13.62l0.97,10.83l15.11,4.07l0.03-0.01l15.11-4.08l1.57-17.59l-47.01,0 l-3.65-41.02h67.94L86.89,36.18L86.89,36.18L86.89,36.18L86.89,36.18z"/></g></svg>`,
         serviceName:"Backend Development",
-        serviceDesc:"Description about the service goes here."
+        serviceDesc:"I can build robust REST APIs and server-side logic using Flask, Django and NodeJS, ensuring your data is stored securely and flows correctly to your front-end."
     },{
         serviceIcon:`<?xml version="1.0" encoding="utf-8"?><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 108.36 122.88" style="enable-background:new 0 0 108.36 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M0,0l9.86,110.59l44.25,12.29l44.37-12.3L108.36,0L0,0L0,0L0,0z M86.89,36.18h-51.9l1.24,13.89l49.43,0 l-3.72,41.64l-27.82,7.71l-27.79-7.71l-1.9-21.31h13.62l0.97,10.83l15.11,4.07l0.03-0.01l15.11-4.08l1.57-17.59l-47.01,0 l-3.65-41.02h67.94L86.89,36.18L86.89,36.18L86.89,36.18L86.89,36.18z"/></g></svg>`,
         serviceName:"Figma to Code",
-        serviceDesc:"Description about the service goes here."
+        serviceDesc:"I can take your Figma wireframes and translate them into pixel-perfect, responsive HTML/CSS or React components, bringing your vision to the browser exactly as intended."
     },
 ]
